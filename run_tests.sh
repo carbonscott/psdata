daf614dcf3afbedf9a7dcb1c6f27228e74f644d3
carbonscott <wangimagine@gmail.com>
2026-07-11 16:15:12 +0000

    test(fail-04): register test_fail04_truncated_index.py in the suite list

--- run_tests.sh
+++ run_tests.sh
@@ -64,6 +64,7 @@ if [[ ${#TESTS[@]} -eq 0 && "${PSDATA_NO_DEFAULT_TESTS:-}" != "1" ]]; then
     "$REPO/tests/test_index_format_idx02.py"
     "$REPO/tests/test_idx03_portable_index.py"
     "$REPO/tests/test_robust_us004.py"
+    "$REPO/tests/test_fail04_truncated_index.py"
     "$REPO/tests/test_str04_chunk_gap.py"
     "$REPO/tests/test_regression_us005.py"
     "$REPO/tests/test_persist_us008.py"
