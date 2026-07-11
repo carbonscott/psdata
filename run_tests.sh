fbad5392c628fa4c736806ccc62514ea0aa318ce
carbonscott <wangimagine@gmail.com>
2026-07-11 14:46:56 +0000

    test(fail-06): register test_hostile_bytes_fail06.py in the suite list

--- run_tests.sh
+++ run_tests.sh
@@ -53,6 +53,7 @@ TESTS=("$@")
 if [[ ${#TESTS[@]} -eq 0 && "${PSDATA_NO_DEFAULT_TESTS:-}" != "1" ]]; then
   TESTS=(
     "$REPO/tests/test_format_us001.py"
+    "$REPO/tests/test_hostile_bytes_fail06.py"
     "$REPO/tests/test_stream_us002.py"
     "$REPO/tests/test_gate02_gated_forward.py"
     "$REPO/tests/test_index_us003.py"
