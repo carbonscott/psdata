90292db6ef7bad9eb49d9e3bbd8ea19ebfc5d6d3
carbonscott <wangimagine@gmail.com>
2026-07-11 16:15:50 +0000

    test(mem-01): register test_mem01_bounded_read.py in the suite list

--- run_tests.sh
+++ run_tests.sh
@@ -69,6 +69,7 @@ if [[ ${#TESTS[@]} -eq 0 && "${PSDATA_NO_DEFAULT_TESTS:-}" != "1" ]]; then
     "$REPO/tests/test_regression_us005.py"
     "$REPO/tests/test_persist_us008.py"
     "$REPO/tests/test_batch_us009.py"
+    "$REPO/tests/test_mem01_bounded_read.py"
     "$REPO/tests/test_config_us010.py"
     "$REPO/tests/test_torch_us011.py"
     "$REPO/tests/test_bigdata_scan_us012.py"
